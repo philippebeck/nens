2b2f1895d22c92fd7cd3ada8e239277c5bbdbf65
Philippe Beck <philippe@philippebeck.net>
2023-12-01 19:07:20 +0400

    Add sequelize to app

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 "use strict";
 
-const express = require("express");
-const helmet  = require("helmet");
-const cors    = require("cors");
+const express   = require("express");
+const cors      = require("cors");
+const helmet    = require("helmet");
+const Sequelize = require('sequelize');
 
 const articleRoute  = require("./route/ArticleRoute");
 const authRoute     = require("./route/AuthRoute");
@@ -16,23 +17,34 @@ const userRoute     = require("./route/UserRoute");
 require("dotenv").config();
 
 /**
+ * SEQUELIZE
+ */
+const sequelize = new Sequelize(process.env.DB);
+sequelize
+  .authenticate()
+  .then(() => { console.log(process.env.SQL_OK) })
+  .catch(err => { console.error(process.env.SQL_KO, err) });
+
+/**
  * EXPRESS
  */
 const app = express();
-app.use(express.json());
-app.use(cors());
-app.use(helmet());
+app
+  .use(express.json())
+  .use(cors())
+  .use(helmet());
 
 /**
  * ROUTES
  */
-app.use(process.env.ROUTE_ARTICLE, articleRoute);
-app.use(process.env.ROUTE_AUTH, authRoute);
-app.use(process.env.ROUTE_GALLERY, galleryRoute);
-app.use(process.env.ROUTE_IMAGE, imageRoute);
-app.use(process.env.ROUTE_LINK, linkRoute);
-app.use(process.env.ROUTE_ORDER, orderRoute);
-app.use(process.env.ROUTE_PRODUCT, productRoute);
-app.use(process.env.ROUTE_USER, userRoute);
+app
+  .use(process.env.ROUTE_ARTICLE, articleRoute)
+  .use(process.env.ROUTE_AUTH, authRoute)
+  .use(process.env.ROUTE_GALLERY, galleryRoute)
+  .use(process.env.ROUTE_IMAGE, imageRoute)
+  .use(process.env.ROUTE_LINK, linkRoute)
+  .use(process.env.ROUTE_ORDER, orderRoute)
+  .use(process.env.ROUTE_PRODUCT, productRoute)
+  .use(process.env.ROUTE_USER, userRoute);
 
 module.exports = app;
