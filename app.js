32dc3524e5a3eb2fe3a48545e75a82d105f59506
Philippe Beck <philippe@philippebeck.net>
2023-12-01 22:16:27 +0400

    Refacto app

--- app.js
+++ app.js
@@ -1,50 +1,26 @@
 "use strict";
 
-const express   = require("express");
-const cors      = require("cors");
-const helmet    = require("helmet");
-const Sequelize = require('sequelize');
-
-const articleRoute  = require("./route/ArticleRoute");
-const authRoute     = require("./route/AuthRoute");
-const galleryRoute  = require("./route/GalleryRoute");
-const imageRoute    = require("./route/ImageRoute");
-const linkRoute     = require("./route/LinkRoute");
-const orderRoute    = require("./route/OrderRoute");
-const productRoute  = require("./route/ProductRoute");
-const userRoute     = require("./route/UserRoute");
+const express = require("express");
+const cors    = require("cors");
+const helmet  = require("helmet");
 
 require("dotenv").config();
 
-/**
- * SEQUELIZE
- */
-const sequelize = new Sequelize(process.env.DB);
-sequelize
-  .authenticate()
-  .then(() => { console.log(process.env.SQL_OK) })
-  .catch(err => { console.error(process.env.SQL_KO, err) });
-
-/**
- * EXPRESS
- */
 const app = express();
+
 app
   .use(express.json())
   .use(cors())
   .use(helmet());
 
-/**
- * ROUTES
- */
 app
-  .use(process.env.ROUTE_ARTICLE, articleRoute)
-  .use(process.env.ROUTE_AUTH, authRoute)
-  .use(process.env.ROUTE_GALLERY, galleryRoute)
-  .use(process.env.ROUTE_IMAGE, imageRoute)
-  .use(process.env.ROUTE_LINK, linkRoute)
-  .use(process.env.ROUTE_ORDER, orderRoute)
-  .use(process.env.ROUTE_PRODUCT, productRoute)
-  .use(process.env.ROUTE_USER, userRoute);
+  .use(process.env.ROUTE_ARTICLE, require("./route/ArticleRoute"))
+  .use(process.env.ROUTE_AUTH, require("./route/AuthRoute"))
+  .use(process.env.ROUTE_GALLERY, require("./route/GalleryRoute"))
+  .use(process.env.ROUTE_IMAGE, require("./route/ImageRoute"))
+  .use(process.env.ROUTE_LINK, require("./route/LinkRoute"))
+  .use(process.env.ROUTE_ORDER, require("./route/OrderRoute"))
+  .use(process.env.ROUTE_PRODUCT, require("./route/ProductRoute"))
+  .use(process.env.ROUTE_USER, require("./route/UserRoute"));
 
 module.exports = app;
