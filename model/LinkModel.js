764c198b73b5699a6d5ebc0e0fbb6ef85ed94924
Philippe Beck <philippe@philippebeck.net>
2023-12-01 16:53:18 +0400

    Create LinkModel for SQL

--- model/LinkModel.js
+++ model/LinkModel.js
@@ -1,1 +1,40 @@
 "use strict";
+
+const { Sequelize, DataTypes } = require('sequelize');
+
+/**
+ * Define the Link Model
+ *
+ * @param {Sequelize} Sequelize - the Sequelize instance
+ * @param {DataTypes} DataTypes - the DataTypes instance
+ * @return {LinkModel} the Link Model
+ */
+module.exports = (Sequelize, DataTypes) => {
+  const LinkModel = Sequelize.define('Links', {
+
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+
+    url: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+
+    cat: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  });
+
+  return LinkModel;
+}
