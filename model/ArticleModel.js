dfa4ccef186292c3d350091adbb6036c16cee82c
Philippe Beck <philippe@philippebeck.net>
2023-12-01 16:52:46 +0400

    Create ArticleModel for SQL

--- model/ArticleModel.js
+++ model/ArticleModel.js
@@ -1,2 +1,67 @@
 "use strict";
 
+const { Sequelize, DataTypes } = require('sequelize');
+
+/**
+ * Define the Article Model
+ *
+ * @param {Sequelize} Sequelize - the Sequelize instance
+ * @param {DataTypes} DataTypes - the DataTypes instance
+ * @return {ArticleModel} the Article Model
+ */
+module.exports = (Sequelize, DataTypes) => {
+  const ArticleModel = Sequelize.define('Articles', {
+
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+
+    text: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+
+    alt: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+
+    likes: {
+      type: DataTypes.JSON,
+      allowNull: false
+    },
+
+    cat: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+
+    created: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+
+    updated: {
+      type: DataTypes.DATE,
+      allowNull: false
+    }
+  });
+
+  return ArticleModel;
+}
