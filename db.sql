2267490e87fff11c6f0635563e0b763aa254a562
Philippe Beck <philippe@philippebeck.net>
2023-12-01 16:52:25 +0400

    Add missing unique to DB

--- db.sql
+++ db.sql
@@ -22,7 +22,7 @@ CREATE TABLE Galleries
   id      SMALLINT      UNSIGNED  PRIMARY KEY AUTO_INCREMENT,
   name    VARCHAR(100)  NOT NULL  UNIQUE,
   author  VARCHAR(100)  NOT NULL,
-  cover   VARCHAR(100)  NOT NULL
+  cover   VARCHAR(100)  NOT NULL  UNIQUE
 )
 ENGINE=INNODB DEFAULT CHARSET=utf8;
 
@@ -76,7 +76,7 @@ CREATE TABLE Orders
   id          SMALLINT     UNSIGNED     PRIMARY KEY AUTO_INCREMENT,
   products    JSON         NOT NULL,
   total       DECIMAL      NOT NULL,
-  payment_id  VARCHAR(50)  NOT NULL,
+  payment_id  VARCHAR(50)  NOT NULL     UNIQUE,
   status      VARCHAR(10)  NOT NULL,
   user_id     SMALLINT     UNSIGNED     NOT NULL,
   created     DATETIME     NOT NULL,
