6b9f5b4c140d8091715e27f25d0f74e60ffb5438
Philippe Beck <philippe@philippebeck.net>
2023-12-24 22:37:02 +0400

    Switch GalleryCtrl from promise to async/await

--- controller/GalleryCtrl.js
+++ controller/GalleryCtrl.js
@@ -7,12 +7,13 @@ const nem         = require("nemjs");
 
 require("dotenv").config();
 
-const { GALLERIES_NOT_FOUND } = process.env;
+const { GALLERIES_NOT_FOUND, IMG_URL, THUMB_URL } = process.env;
 
-const GALLERIES_IMG   = process.env.IMG_URL + "galleries/";
-const GALLERIES_THUMB = process.env.THUMB_URL + "galleries/";
+const GALLERIES_IMG   = `${IMG_URL}galleries/`;
+const GALLERIES_THUMB = `${THUMB_URL}galleries/`;
 
-const form    = formidable();
+const form = formidable();
+
 const Gallery = db.gallery;
 const Image   = db.image;
 
@@ -20,7 +21,8 @@ const Image   = db.image;
 
 /**
  * ? CHECK GALLERY DATA
- * * Checks the gallery data for valid name and author.
+ * * Checks the gallery data for valid name & author.
+ * 
  * @param {string} name - The name of the gallery.
  * @param {string} author - The author of the gallery.
  * @param {object} res - The response object.
@@ -29,10 +31,10 @@ const Image   = db.image;
 exports.checkGalleryData = (name, author, res) => {
   const { CHECK_NAME, STRING_MAX, STRING_MIN } = process.env;
 
-  if (
-    !nem.checkRange(author, STRING_MIN, STRING_MAX) || 
-    !nem.checkRange(name, STRING_MIN, STRING_MAX)
-    ) {
+  const IS_NAME_CHECKED   = nem.checkRange(name, STRING_MIN, STRING_MAX);
+  const IS_AUTHOR_CHECKED = nem.checkRange(author, STRING_MIN, STRING_MAX);
+
+  if (!IS_NAME_CHECKED || !IS_AUTHOR_CHECKED) {
     return res.status(403).json({ message: CHECK_NAME });
   }
 }
@@ -40,6 +42,7 @@ exports.checkGalleryData = (name, author, res) => {
 /**
  * ? CHECK GALLERY UNIQUE
  * * Checks if the given name is unique in the gallery.
+ * 
  * @param {string} name - The name to be checked.
  * @param {object} gallery - The gallery object.
  * @param {object} res - The response object.
@@ -57,129 +60,164 @@ exports.checkGalleryUnique = (name, gallery, res) => {
 /**
  * ? LIST GALLERIES
  * * Retrieves a list of all galleries.
+ * 
  * @param {Object} req - The request object.
  * @param {Object} res - The response object.
  * @return {Object} The list of galleries in JSON format.
  * @throws {Error} If the galleries are not found in the database.
  */
-exports.listGalleries = (req, res) => {
-  Gallery.findAll()
-    .then((galleries) => { res.status(200).json(galleries) })
-    .catch(() => res.status(404).json({ message: GALLERIES_NOT_FOUND }));
+exports.listGalleries = async (req, res) => {
+  try {
+    const galleries = await Gallery.findAll();
+    res.status(200).json(galleries);
+
+  } catch (error) {
+    console.error(error);
+    res.status(404).json({ message: GALLERIES_NOT_FOUND });
+  }
 }
 
 /**
  * ? READ GALLERY
- * * Retrieves a gallery by its ID and sends it as a JSON response.
+ * * Retrieves a gallery by its ID.
+ * 
  * @param {object} req - The request object.
  * @param {object} res - The response object.
  * @return {object} The retrieved gallery as a JSON response.
  * @throws {Error} If the gallery is not found in the database.
  */
-exports.readGallery = (req, res) => {
+exports.readGallery = async (req, res) => {
   const { GALLERY_NOT_FOUND } = process.env;
   const ID = parseInt(req.params.id, 10);
 
-  Gallery.findByPk(ID)
-    .then((gallery) => { res.status(200).json(gallery) })
-    .catch(() => res.status(404).json({ message: GALLERY_NOT_FOUND }));
+  try {
+    const gallery = await Gallery.findByPk(ID);
+    res.status(200).json(gallery);
+
+  } catch (error) {
+    console.error(error);
+    res.status(404).json({ message: GALLERY_NOT_FOUND });
+  }
 }
 
 //! ******************** PRIVATE ********************
 
 /**
  * ? CREATE GALLERY
- * * Creates a new gallery.
+ * * Creates a new gallery based on the request data.
+ * 
  * @param {Object} req - the request object
  * @param {Object} res - the response object
  * @param {Function} next - the next middleware function
  * @return {Object} A message indicating that the gallery was created.
  * @throws {Error} If the gallery was not created.
  */
-exports.createGallery = (req, res, next) => {
+exports.createGallery = async (req, res, next) => {
   const { GALLERY_CREATED, GALLERY_NOT_CREATED } = process.env;
 
-  form.parse(req, (err, fields) => {
+  form.parse(req, async (err, fields) => {
     if (err) { next(err); return }
 
     const { author, name } = fields;
-    this.checkGalleryData(name, author, res);
+    const cover = nem.getPosterName(name);
 
-    Gallery.findAll()
-      .then((galleries) => {
-        for (let gallery of galleries) this.checkGalleryUnique(name, gallery, res);
+    try {
+      this.checkGalleryData(name, author, res);
 
-        const cover   = nem.getPosterName(name);
-        const gallery = { ...fields, cover: cover };
+      const galleries = await Gallery.findAll();
 
-        Gallery.create(gallery)
-          .then(() => res.status(201).json({ message: GALLERY_CREATED }))
-          .catch(() => res.status(400).json({ message: GALLERY_NOT_CREATED }));
-      })
-      .catch(() => res.status(404).json({ message: GALLERIES_NOT_FOUND }));
+      if (!galleries || galleries.length === 0) {
+        return res.status(404).json({ message: GALLERIES_NOT_FOUND });
+      }
+
+      for (const gallery of galleries) {
+        this.checkGalleryUnique(name, gallery, res);
+      }
+
+      await Gallery.create({ ...fields, cover });
+      res.status(201).json({ message: GALLERY_CREATED });
+
+    } catch (error) {
+      console.error(error);
+      res.status(400).json({ message: GALLERY_NOT_CREATED });
+    }
   })
 }
 
 /**
  * ? UPDATE GALLERY
- * * Update the gallery with the given request data.
+ * * Update the gallery by its ID & based on the request data.
+ * 
  * @param {Object} req - The request object.
  * @param {Object} res - The response object.
  * @param {Function} next - The next middleware function.
  * @return {Object} A message indicating that the gallery was updated.
  * @throws {Error} If the gallery is not updated.
  */
-exports.updateGallery = (req, res, next) => {
+exports.updateGallery = async (req, res, next) => {
   const { GALLERY_NOT_UPDATED, GALLERY_UPDATED } = process.env;
   const ID = parseInt(req.params.id, 10);
 
-  form.parse(req, (err, fields) => {
+  form.parse(req, async (err, fields) => {
     if (err) { next(err); return }
 
     const { author, name } = fields;
-    this.checkGalleryData(name, author, res);
 
-    Gallery.findAll()
-      .then((galleries) => {
-        galleries.filter(gallery => gallery.id !== ID).forEach(gallery => 
-          this.checkGalleryUnique(name, gallery, res));
+    try {
+      this.checkGalleryData(name, author, res);
 
-        const gallery = { ...fields };
+      const galleries = await Gallery.findAll();
 
-        Gallery.update(gallery, { where: { id: ID }})
-          .then(() => res.status(200).json({ message: GALLERY_UPDATED }))
-          .catch(() => res.status(400).json({ message: GALLERY_NOT_UPDATED }));
-      })
-      .catch(() => res.status(404).json({ message: GALLERIES_NOT_FOUND }));
+      if (!galleries || galleries.length === 0) {
+        return res.status(404).json({ message: GALLERIES_NOT_FOUND });
+      }
+
+      galleries
+        .filter((gallery) => gallery.id !== ID)
+        .forEach((gallery) => this.checkGalleryUnique(name, gallery, res));
+
+      await Gallery.update({ ...fields }, { where: { id: ID } });
+      res.status(200).json({ message: GALLERY_UPDATED });
+
+    } catch (error) {
+      console.error(error);
+      res.status(400).json({ message: GALLERY_NOT_UPDATED });
+    }
   })
 }
 
 /**
  * ? DELETE GALLERY
- * * Deletes a gallery and its associated images.
+ * * Deletes a gallery by its ID.
+ * 
  * @param {Object} req - the request object
  * @param {Object} res - the response object
  * @return {Object} A message indicating that the gallery was deleted.
  * @throws {Error} If the gallery is not deleted.
  */
-exports.deleteGallery = (req, res) => {
-  const { GALLERY_DELETED, GALLERY_NOT_DELETED, IMAGE_DELETE_MANY, IMAGES_NOT_FOUND } = process.env;
+exports.deleteGallery = async (req, res) => {
+  const { GALLERY_DELETED, GALLERY_NOT_DELETED, IMAGES_NOT_FOUND } = process.env;
   const ID = parseInt(req.params.id, 10);
 
-  Image.findAll({ where: { galleryId: ID }})
-    .then(images => {
-      for (let image of images) {
-        fs.unlink(GALLERIES_THUMB + image.name, () => { 
-          fs.unlink(GALLERIES_IMG + image.name, () => {})
-        });
-      }
-      Image.destroy({ where: { galleryId: ID }})
-        .then(() =>
-          Gallery.destroy({ where: { id: ID }})
-            .then(() => res.status(204).json({ message: GALLERY_DELETED }))
-            .catch(() => res.status(400).json({ message: GALLERY_NOT_DELETED }))
-        )
-        .catch(() => res.status(400).json({ message: IMAGE_DELETE_MANY }));
-    })
-    .catch(() => res.status(404).json({ message: IMAGES_NOT_FOUND }));
+  try {
+    const images = await Image.findAll({ where: { galleryId: ID }});
+
+    if (!images || images.length === 0) {
+      return res.status(404).json({ message: IMAGES_NOT_FOUND });
+    }
+
+    for (const image of images) {
+      await fs.promises.unlink(GALLERIES_THUMB + image.name);
+      await fs.promises.unlink(GALLERIES_IMG + image.name);
+    }
+
+    await Image.destroy({ where: { galleryId: ID }});
+    await Gallery.destroy({ where: { id: ID }});
+
+    res.status(204).json({ message: GALLERY_DELETED });
+
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ message: GALLERY_NOT_DELETED });
+  }
 }
